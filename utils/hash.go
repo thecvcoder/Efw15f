1a36e374951609651cb63c0a0ca3e54788631205
thecvcoder <thecvcoder@foxmail.com>
2022-12-02 21:47:35 -0500

    密码hash加密

---
+++ utils/hash.go
@@ -0,0 +1,15 @@
+package utils
+
+import "golang.org/x/crypto/bcrypt"
+
+// BcryptHash 密码hash加密
+func BcryptHash(password string) string {
+	bytes, _ := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	return string(bytes)
+}
+
+// BcryptCheck 对比明文和数据库的哈希值
+func BcryptCheck(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
+}
